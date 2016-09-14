5b003c642266737771d7e0d374bf78bda44c42b4
Marijn Koesen <github@koesen.nl>
2016-09-14 15:44:12 +0200

    Made RunnableExample compilable by fixing imports

--- example/RunnableExample.java
+++ example/RunnableExample.java
@@ -2,8 +2,8 @@ package de.daslaboratorium.machinelearning.bayes.example;
 
 import java.util.Arrays;
 
-import de.daslaboratorium.machinelearning.bayes.BayesClassifier;
-import de.daslaboratorium.machinelearning.bayes.Classifier;
+import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
+import de.daslaboratorium.machinelearning.classifier.Classifier;
 
 public class RunnableExample {
 
